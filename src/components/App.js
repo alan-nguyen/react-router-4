2e9ad6d127fe3ba379963a428d4ae79fd2c4c2b2
Alan Nguyen <tuan8101@gmail.com>
2021-01-08 22:24:05 -0700

    Render Route for About

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 // App components
 import Home from './Home';
+import About from './About';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   </BrowserRouter>
 );
